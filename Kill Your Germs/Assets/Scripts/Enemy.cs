8b7186732d667b9837c0c6aae5699a81d50efd2a
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2018-11-19 17:02:27 +0200

    used navmeshagent

--- Kill Your Germs/Assets/Scripts/Enemy.cs
+++ Kill Your Germs/Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,20 +10,25 @@ public class Enemy : MonoBehaviour
     public GameObject player;
     public float speed = 0.01f;
     public Image healthbar;
+    private NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        agent = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(player.transform);
+        //transform.LookAt(player.transform);
 
-        transform.position += transform.forward * speed * Time.deltaTime;
-        transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
+        //transform.position += transform.forward * speed * Time.deltaTime;
+        //transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
+
+        agent.destination = player.transform.position;
+        
+        
         //transform.position.Set(transform.position.x, 2.0f, transform.position.z);
 
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z), speed * Time.deltaTime);
