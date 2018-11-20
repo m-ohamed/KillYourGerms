0c60958a84aea8ad1727bbc1394047cf6148e2f2
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2018-11-20 13:19:15 +0200

    added main menu, enhanced enemy spawn

--- Kill Your Germs/Assets/Scripts/enemySpawner.cs
+++ Kill Your Germs/Assets/Scripts/enemySpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class enemySpawner : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject player;
+
     private float timer = 5.0f;
 
 	// Use this for initialization
@@ -18,11 +20,31 @@ public class enemySpawner : MonoBehaviour
     {
         timer -= Time.deltaTime;
 
-        float randX = Random.Range(-30.0f, 30.0f);
-        float randZ = Random.Range(-30.0f, 30.0f);
 
         if (timer <= 0.0f)
         {
+            float randX = Random.Range(-30.0f, 30.0f);
+            float randZ = Random.Range(-30.0f, 30.0f);
+
+          
+            if(Mathf.Abs(randX - player.transform.position.x) <= 4.0f)
+            {
+                if (randX < player.transform.position.x)
+                    randX -= 5.0f;
+                else
+                    randX += 5.0f;
+            }
+        
+            if(Mathf.Abs(randZ - player.transform.position.z) <= 4.0f)
+            {
+                if(randZ < player.transform.position.z)
+                    randZ -= 5.0f;
+                else
+                    randZ += 5.0f;
+            }
+        
+            
+
             GameObject newEnemy = Instantiate(enemy, new Vector3(randX, 1.0f, randZ), transform.rotation);
             newEnemy.SetActive(true);
             timer = 5.0f;
---
+++ Kill Your Germs/Assets/Scripts/muteScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class muteScript : MonoBehaviour
+{
+    public AudioMixer audio;
+    private bool isMute = false;
+
+	public void muteSound()
+    {
+        if(isMute)
+        {
+            audio.SetFloat("mainVolume", 0);
+            isMute = false;
+        }
+        else
+        {
+            audio.SetFloat("mainVolume", -80);
+            isMute = true;
+        }
+        
+    }
+}
